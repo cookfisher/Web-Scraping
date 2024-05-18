9f78cf36ca7ca4bca4af908ed2836dcc5472171a
cookfisher <cookfisher@outlook.com>
2024-05-18 17:44:48 -0400

    parse data and store data in local folder and mongodb

--- fm/fm/pipelines.py
+++ fm/fm/pipelines.py
@@ -5,9 +5,34 @@
 
 
 # useful for handling different item types with a single interface
+import os
+import pymongo
 from itemadapter import ItemAdapter
 
 
 class FmPipeline:
     def process_item(self, item, spider):
-        return item
+        download_path = os.getcwd() + '/download/'
+        if not os.path.exists(download_path):
+            os.mkdir(download_path)
+        type_ = item.get('type')
+        if type_ == 'image':
+            image_name = item.get('image_name')
+            image_data = item.get('image_data')
+            with open(download_path + image_name, 'wb') as f:
+                f.write(image_data)
+                print("Saved image: ", image_name)
+        elif type_ == 'info':
+            mongo_client = pymongo.MongoClient()
+            collection = mongo_client['py_spider']['qingtingFM']
+            collection.insert_one(item)
+            print("Saved info: ", item.get('title'))
+        else:
+            print('Invalid data type...')
+
+
+
+
+
+
+
--- fm/fm/spiders/qingting.py
+++ fm/fm/spiders/qingting.py
@@ -5,12 +5,36 @@ from scrapy.http import HtmlResponse
 
 class QingtingSpider(scrapy.Spider):
     name = "qingting"
-    allowed_domains = ["m.qingting.fm"]
+    allowed_domains = ["m.qingting.fm", "pic.qtfm.cn"]
     start_urls = ["https://m.qingting.fm/rank/"]
 
     def parse(self, response: HtmlResponse, **kwargs):
-        print(response.text)
+        a_list = response.xpath('//div[@class="rank-list"]/a')
+        for l in a_list:
+            rank_number = l.xpath('./div[@class="badge"]/text()').extract_first()
+            img_url = l.xpath('./img/@src').extract_first()
+            title = l.xpath('./div[@class="content"]/div[@class="title"]/text()').extract_first()
+            desc = l.xpath('./div[@class="content"]/div[@class="desc"]/text()').extract_first()
+            play_number = l.xpath('.//div[@class="info-item"][1]/span/text()').extract_first()
+
+            yield {
+                'type': 'info',
+                'rank_number': rank_number,
+                'img_url': img_url,
+                'title': title,
+                'desc': desc,
+                'play_number': play_number
+            }
+
+            yield scrapy.Request(img_url, callback=self.parse_image, cb_kwargs={'image_name': title})
+
+    def parse_image(self, response, image_name):
+        yield {
+            'type': 'image',
+            'image_name': image_name + ".png",
+            'image_data': response.body
+        }
 
 
 if __name__ == '__main__':
-    cmdline.execute('scrapy crawl qingting'.split())
+    cmdline.execute('scrapy crawl qingting --nolog'.split())
